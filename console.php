71c22c16bc76d989c614db4969870d8947b583f1
Thomas Bibard <thomas.bibard@neblion.net>
2019-08-15 08:44:33 +0200

    Add/update cmd préparation tables

--- console.php
+++ console.php
@@ -6,8 +6,10 @@ require __DIR__.'/vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use NblSpreadsheet\ImportPopulationCommand;
 use NblSpreadsheet\GetFilesPopulationCommand;
+use NblSpreadsheet\PrepareDatabasePopulationCommand;
 
 $application = new Application('NblSpreadsheet App', '0.0.1');
 $application->add(new ImportPopulationCommand());
 $application->add(new GetFilesPopulationCommand());
+$application->add(new PrepareDatabasePopulationCommand());
 $application->run();
